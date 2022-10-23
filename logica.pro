abe4513810cb21b72f2bca2fb23da35edb724eec
jphuntv <jeanpaulhunt16@gmail.com>
2022-10-23 01:11:08 -0600

    algoritmos de movimiento y laberinto funcional en consola

--- logica.pro
+++ logica.pro
@@ -1,3 +1,30 @@
+movimientoValido(ad, d).
+movimientoValido(ab, s).
+movimientoValido(at, a).
+movimientoValido(ar, w).
+
+movimientoValido(inter, w).
+movimientoValido(inter, s).
+movimientoValido(inter, a).
+movimientoValido(inter, d).
+
+movimientoValido(i, w).
+movimientoValido(i, s).
+movimientoValido(i, a).
+movimientoValido(i, d).
+
+posValida(i).
+posValida(f).
+posValida(ad).
+posValida(at).
+posValida(ab).
+posValida(ar).
+posValida(inter).
+
+permiteMovimiento(Ficha,Direccion,Siguiente):-
+    movimientoValido(Ficha,Direccion),
+    posValida(Siguiente).
+
 splitFile(Archivo,X) :-
     read_file_to_string(Archivo, Res,[]),%archivo a string
     split_string(Res,".\n", ".\n", X). %string splitter
--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 
+from glob import glob
+from turtle import pos
 from pyswip import Prolog
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import *
 
+from tabulate import tabulate
+
 
 ventana = Tk()
 prolog = Prolog()
 prolog.consult("logica.pro")
+
 rutaArchivoLaberinto = ""
 laberinto = []
+
+posX = 0
+posY = 0
+fichaAnterior = "i"
 
 def solicitarArchivo():
     global archivoLaberinto
     print ("solicitar archivo")
-    rutaArchivoLaberinto = filedialog.askopenfilename()
-    laberintoProlog = prolog.query("getLaberinto('%s',X)." % (rutaArchivoLaberinto))
+    #rutaArchivoLaberinto = filedialog.askopenfilename()
+    #laberintoProlog = prolog.query("getLaberinto('%s',X)." % (rutaArchivoLaberinto))
+    laberintoProlog = prolog.query("getLaberinto('laberinto1.txt',X).")
     transformarLaberinto(laberintoProlog)
+    obtenerPosicionInicial()
+    verLaberinto()
 
 def transformarLaberinto(laberintoProlog):
     global laberinto
@@ -26,32 +38,95 @@ def transformarLaberinto(laberintoProlog):
             listaEle = elemento.strip('][').split(',') #transformar de string a lista
             laberinto += [listaEle]
 
-
+def obtenerPosicionInicial():
+    global laberinto, posX, posY
+    x = 0
+    y = 0
+    for i in laberinto:
+        for j in i:
+            if j == 'i':
+                posX = x
+                posY = y
+                laberinto[posX][posY]="O"
+            y+=1
+        x+=1
+        y=0
+    print("x= ",posX," y= ",posY)
 
 
 
 botonArchivo= tk.Button(ventana, text ="Archivo", command = solicitarArchivo)
 botonArchivo.pack()
 
+def verificarGane():
+    if fichaAnterior =="f":
+        print("Ganó")
 
 def verLaberinto():
     global laberinto
     print("laberinto---------------------")
-    for fila in laberinto:
-        print(fila)
+    print(tabulate(laberinto, tablefmt="grid"))
+    verificarGane()
 
 botonVer= tk.Button(ventana, text ="ver", command = verLaberinto)
 botonVer.pack()
 
 verLaberinto()
 def moverIzquierda(event):
-    print("Izquierda")
+    global posX, posY,fichaAnterior
+    print("Movimiento: Izquirda\n")
+    print("actual: ",fichaAnterior,"\tsiguiente: ",laberinto[posX][posY-1])
+    movimientoValido = bool(list(prolog.query("permiteMovimiento(%s,a,%s)."%(fichaAnterior, laberinto[posX][posY-1]))))
+    print(movimientoValido)
+    if posY-1 >= 0 and movimientoValido:
+        fichaTemp = fichaAnterior
+        fichaAnterior = laberinto[posX][posY-1]
+        laberinto[posX][posY] = fichaTemp
+        laberinto[posX][posY-1] = 'O'
+        posY-=1
+    verLaberinto()
+
 def moverDerecha(event):
-    print("Derecha")
+    global posX, posY,fichaAnterior
+    print("Movimiento: Derecha\n")
+    print("actual: ",fichaAnterior,"\tsiguiente: ",laberinto[posX][posY+1])
+    movimientoValido = bool(list(prolog.query("permiteMovimiento(%s,d,%s)."%(fichaAnterior, laberinto[posX][posY+1]))))
+    print(movimientoValido)
+    if posY+1 < len(laberinto[posX]) and movimientoValido:
+        fichaTemp = fichaAnterior
+        fichaAnterior = laberinto[posX][posY+1]
+        laberinto[posX][posY] = fichaTemp
+        laberinto[posX][posY+1] = 'O'
+        posY+=1
+    verLaberinto()
+
 def moverArriba(event):
-    print("Arriba")
+    global posX, posY,fichaAnterior
+    print("Movimiento: Arriba\n")
+    print("actual: ",fichaAnterior,"\tsiguiente: ",laberinto[posX-1][posY])
+    movimientoValido = bool(list(prolog.query("permiteMovimiento(%s,w,%s)."%(fichaAnterior, laberinto[posX-1][posY]))))
+    print(movimientoValido)
+    if posX-1 >= 0 and movimientoValido:
+        fichaTemp = fichaAnterior
+        fichaAnterior = laberinto[posX-1][posY]
+        laberinto[posX][posY] = fichaTemp
+        laberinto[posX-1][posY] = 'O'
+        posX-=1
+    verLaberinto()
+
 def moverAbajo(event):
-    print("Abajo")
+    global posX, posY,fichaAnterior
+    print("Movimiento: abajo\n")
+    print("actual: ",fichaAnterior,"\tsiguiente: ",laberinto[posX+1][posY])
+    movimientoValido = bool(list(prolog.query("permiteMovimiento(%s,s,%s)."%(fichaAnterior, laberinto[posX+1][posY]))))
+    print(movimientoValido)
+    if posX+1 < len(laberinto) and movimientoValido:
+        fichaTemp = fichaAnterior
+        fichaAnterior = laberinto[posX+1][posY]
+        laberinto[posX][posY] = fichaTemp
+        laberinto[posX+1][posY] = 'O'
+        posX+=1
+    verLaberinto()
 
  
  
